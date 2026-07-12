7aa0337b71db772d69ba31f3ee16c950292f157c
aastha sharma <asthasharma1012@gmail.com>
2026-07-12 16:11:36 +0530

    fixed blank clocl file with actual code

--- clock.py
+++ clock.py
@@ -1,2 +1,15 @@
+import time
 
+current_time=time.localtime()  
+
+twelve_hour_format=time.strftime("%I:%M:%S %p")
+print(f"current time: {twelve_hour_format}")
+if current_time.tm_hour>=0 and current_time.tm_hour<12:
+    print("good morning")
+elif current_time.tm_hour>=12 and current_time.tm_hour<18:
+    print("good afternoon")
+elif current_time.tm_hour>=18 and current_time.tm_hour<24:
+    print("good evening")    
+else:
+    print("good night")
 
